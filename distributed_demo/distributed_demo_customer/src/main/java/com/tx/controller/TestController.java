6f07bbb33b7904ede22882ec5e99b2ae9072a579
田旋 <tianxuan@sandwish.cn>
2016-04-04 22:59:45 +0800

    使用spring session管理所有会话,这里的实现是通过spring session框架,采用redis实现会话的集中式管理,支持

--- distributed_demo/distributed_demo_customer/src/main/java/com/tx/controller/TestController.java
+++ distributed_demo/distributed_demo_customer/src/main/java/com/tx/controller/TestController.java
@@ -2,7 +2,12 @@ package com.tx.controller;
 
 import com.tx.dubbo.demo.DemoService;
 import com.tx.service.TestService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +18,10 @@ import javax.jms.Message;
 import javax.jms.TextMessage;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.Charset;
 
 /**
  * Created by tianxuan on 16/2/19.
@@ -24,7 +31,7 @@ import java.io.PrintWriter;
 public class TestController {
     @Autowired
     private  TestService testService;
-    @Autowired
+    @Autowired(required = false)
     private DemoService demoService;
     @Autowired
     JmsTemplate jmsTemplate;
@@ -66,5 +73,14 @@ public class TestController {
             }
         }
 
+    }
+    @RequestMapping("/sessiontest")
+    public ResponseEntity<String> sessionTest(HttpSession session,@RequestParam("name") String name){
+        HttpHeaders headers = new HttpHeaders();
+        MediaType mediaType=new MediaType("text","html", Charset.forName("UTF-8"));
+        headers.setContentType(mediaType);
+        ResponseEntity<String> rs = new ResponseEntity(StringUtils.defaultString(session.getAttribute("name")+"", "session中不存在"),headers,HttpStatus.OK);
+        session.setAttribute("name",name);
+        return  rs;
     }
 }
---
+++ distributed_demo/distributed_demo_customer/src/main/java/com/tx/systemmgr/SessionConfig.java
@@ -0,0 +1,18 @@
+package com.tx.systemmgr;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import redis.clients.jedis.JedisShardInfo;
+
+/**
+ * Created by tianxuan on 16/4/4.
+ */
+@EnableRedisHttpSession
+public class SessionConfig {
+    @Bean
+    public JedisConnectionFactory connectionFactory() {
+        JedisShardInfo shardInfo = new JedisShardInfo("192.168.99.100",9379,"docker-redis");
+        return new JedisConnectionFactory(shardInfo);
+    }
+}
